318beb51e17a9f0d71ec8b100c7e7b038a3b9eee
CVazquezOpalka <cristianvazquez80@gmail.com>
2024-01-16 19:30:58 -0300

    cambios en db

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = "ecomerce.wsgi.application"
 DATABASES = {
     "default": dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default="postgresql://postgres:postgres@localhost:5432/mysite",
+        default="postgresql://postgres:postgres@localhost:5432/ecomerce",
         conn_max_age=600,
     )
 }
