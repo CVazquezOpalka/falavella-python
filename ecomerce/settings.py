c1114b7b2a0b1c981bd5fdac8ef01ba2b1c58c0a
CVazquezOpalka <cristianvazquez80@gmail.com>
2024-01-16 19:35:56 -0300

    vamos que se puede

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -71,11 +71,7 @@ WSGI_APPLICATION = "ecomerce.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default="postgresql://postgres:postgres@localhost:5432/ecomerce",
-        conn_max_age=600,
-    )
+    "default": dj_database_url.config()
 }
 
 
